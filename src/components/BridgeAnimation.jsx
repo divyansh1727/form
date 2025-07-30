ea6e56fa75db0fc9430b10590faa0f43bb57e0f5
divyansh1727 <divys2705@gmail.com>
2025-07-30 20:34:12 +0530

    Delete src directory

--- src/components/BridgeAnimation.jsx
+++
@@ -1,7 +0,0 @@
-export default function BridgeAnimation() {
-  return (
-    <div className="text-center animate-pulse text-pink-700 text-2xl font-medium">
-      🌉 Walking across the love bridge...
-    </div>
-  );
-}
